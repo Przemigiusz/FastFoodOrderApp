aa21f191f24315f9efb2c11cbee0cbbca97470ac
Przemysław Rutkowski <rutkowski_przemyslaw@o2.pl>
2024-05-13 20:41:37 +0200

    Most Popular Products sections bases on web component

--- public/js/mostPopularProducts.js
+++ public/js/mostPopularProducts.js
@@ -1,45 +1,33 @@
 import cache from '../js/cache.js';
 import { addProduct } from './cart.js';
 
-function createOfferItem(product, productType, burgersItems, pizzasItems) {
-    const offerItem = document.createElement('div');
-    const img = document.createElement('img');
-    const title = document.createElement('h3');
-    const description = document.createElement('p');
-    const addToCart = document.createElement('div');
-    const price = document.createElement('p');
-    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+class MenuTabItem extends HTMLElement {
+    constructor() {
+        super();
+    }
 
-    offerItem.className = 'menu-tab-item';
-    img.src = product.image;
-    img.alt = `${product.name} Image`;
-    title.textContent = product.name;
-    description.textContent = product.description;
-    addToCart.className = 'add-to-cart';
-    price.className = 'price';
-    price.textContent = `$${product.price}`;
-    svg.setAttribute('viewBox', '0 0 448 512');
-    path.setAttribute('d', 'M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z');
+    connectedCallback() {
+        const product = JSON.parse(this.getAttribute('product'));
+        const productType = this.getAttribute('productType');
 
-    offerItem.appendChild(img);
-    offerItem.appendChild(title);
-    offerItem.appendChild(description);
-    addToCart.appendChild(price);
-    svg.appendChild(path);
-    addToCart.appendChild(svg);
-    offerItem.appendChild(addToCart);
+        const template = document.getElementById('menu-tab-item-template');
+        const node = document.importNode(template.content, true);
 
-    svg.addEventListener('click', () => {
-        addProduct(product.id, productType);
-    });
+        node.querySelector('img').src = product.image;
+        node.querySelector('img').alt = `${product.name} Image`;
+        node.querySelector('h3').textContent = product.name;
+        node.querySelector('p').textContent = product.description;
+        node.querySelector('.price').textContent = `$${product.price}`;
 
-    if (productType === 'burger') {
-        burgersItems.appendChild(offerItem);
-    } else if (productType === 'pizza') {
-        pizzasItems.appendChild(offerItem);
+        node.querySelector('svg').addEventListener('click', () => {
+            addProduct(product.id, productType);
+        });
+
+        this.appendChild(node);
     }
 }
+
+customElements.define('menu-tab-item', MenuTabItem);
 
 export default function setTopThreeProducts() {
     cache.fetchOffer().then(offer => {
@@ -62,11 +50,17 @@ export default function setTopThreeProducts() {
         mostPopularPizzas.appendChild(pizzasItems);
 
         top3Burgers.forEach(burger => {
-            createOfferItem(burger, 'burger', burgersItems, pizzasItems);
+            const menuTabItem = document.createElement('menu-tab-item');
+            menuTabItem.setAttribute('product', JSON.stringify(burger));
+            menuTabItem.setAttribute('productType', 'burger');
+            burgersItems.appendChild(menuTabItem);
         });
 
         top3Pizzas.forEach(pizza => {
-            createOfferItem(pizza, 'pizza', burgersItems, pizzasItems);
+            const menuTabItem = document.createElement('menu-tab-item');
+            menuTabItem.setAttribute('product', JSON.stringify(pizza));
+            menuTabItem.setAttribute('productType', 'pizza');
+            pizzasItems.appendChild(menuTabItem);
         });
     }).catch(error => console.error('Error:', error));
 }
