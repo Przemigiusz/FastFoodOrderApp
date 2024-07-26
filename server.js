1d31005ccc0d65213a091f1aa6bb890163e7fe99
Przemysław Rutkowski <rutkowski_przemyslaw@o2.pl>
2024-07-26 23:26:37 +0200

    Code refactore and added .gitignore

--- server.js
+++ server.js
@@ -1,46 +1,42 @@
 import http from 'http';
 import fs from 'fs';
 import path from 'path';
-import url from 'url';
+import { fileURLToPath } from 'url';
+
+const rootPath = path.join(path.dirname(fileURLToPath(import.meta.url)));
+
+const mimeTypes = new Map([
+    ['.js', 'text/javascript'],
+    ['.html', 'text/html'],
+    ['.css', 'text/css'],
+    ['.jpg', 'image/jpeg'],
+    ['.png', 'image/png'],
+    ['.jpeg', 'image/jpeg'],
+    ['.webp', 'image/webp'],
+]);
 
 const server = http.createServer((req, res) => {
-    let pathname = url.parse(req.url).pathname;
+    const url = new URL(req.url || '', `http://${req.headers.host}`);
+    const pathname = url.pathname;
     let filePath;
     let contentType;
 
-    if (pathname.startsWith('/images') || pathname.startsWith('/js') || pathname.startsWith('/html') || pathname.startsWith('/css')) {
-        filePath = path.join(process.cwd(), 'public', pathname);
-        switch (path.extname(filePath)) {
-            case '.js':
-                contentType = 'text/javascript';
-                break;
-            case '.css':
-                contentType = 'text/css';
-                break;
-            case '.html':
-                contentType = 'text/html';
-                break;
-            case '.jpg':
-            case '.jpeg':
-            case '.png':
-            case '.gif':
-                contentType = 'image/' + path.extname(filePath).slice(1);
-                break;
-            default:
-                contentType = 'text/plain';
-        }
+    if (pathname === '/') {
+        filePath = path.join(rootPath, 'public/html', 'index.html');
+        contentType = mimeTypes.get('.html');
     } else {
-        filePath = path.join(process.cwd(), 'public/html', 'index.html');
-        contentType = 'text/html';
+        filePath = path.join(rootPath, 'public', pathname);
+        const extension = path.extname(pathname);
+        contentType = mimeTypes.get(extension);
     }
 
     fs.readFile(filePath, (err, content) => {
         if (err) {
-            res.writeHead(500);
-            res.end(`Server Error: ${err.code}`);
+            res.writeHead(404);
+            res.end(`File not found: ${filePath}`);
         } else {
             res.writeHead(200, { 'Content-Type': contentType });
-            res.end(content, 'utf-8');
+            res.end(content);
         }
     });
 });
